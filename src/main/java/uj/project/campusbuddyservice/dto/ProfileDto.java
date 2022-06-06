b202d2b557eaf183f56fefc19901f3fc8b383cb1
LloydBunhle <lloydBunhle@gmail.com>
2022-06-06 15:58:32 +0200

    user details

---
+++ src/main/java/uj/project/campusbuddyservice/dto/ProfileDto.java
@@ -0,0 +1,10 @@
+package uj.project.campusbuddyservice.dto;
+import lombok.Data;
+
+@Data
+public class ProfileDto {
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+}
--- src/main/java/uj/project/campusbuddyservice/repository/UserRepository.java
+++ src/main/java/uj/project/campusbuddyservice/repository/UserRepository.java
@@ -3,12 +3,14 @@ package uj.project.campusbuddyservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uj.project.campusbuddyservice.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);
+    List<User> findByEmail(String email);
     Optional<User> findByUsernameOrEmail(String username, String email);
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+
 }
